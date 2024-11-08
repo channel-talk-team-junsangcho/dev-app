20ddbc19d7621c14a06cb05d0cf5e85a4f96016e
10Hyuna <kimha105@naver.com>
2024-11-09 00:54:59 +0900

    feat: link proxy attendance function

---
+++ server/src/attendaceService.ts
@@ -0,0 +1,35 @@
+import { getSameCourseUser, getSamePeriodCourseUser } from './selectService'
+import handleSendMessageAsBot from '../../wam/src/pages/Send'
+
+const sendAsBotProxyAttendanceMsg = "%s %s 강의 대리 출석해 주세요~!";
+const sendAsBotFailAttendaceMsg = "대리 출석 해 줄 사용자가 없어요...";
+
+interface requestParams {
+    callerId: string, 
+    courseName: string,
+    period: string,
+    day: string
+}
+
+export async function requestProxyAttendance(
+    obj: requestParams
+) {
+    const userId = getAppropriateUser(obj);
+
+    if(userId == null) {
+        handleSendMessageAsBot("", obj.courseName, sendAsBotFailAttendaceMsg);
+    } else {
+        handleSendMessageAsBot("", obj.courseName, sendAsBotProxyAttendanceMsg);
+    }
+}
+
+function getAppropriateUser(
+    obj: requestParams
+) {
+    var userId = getSameCourseUser(obj.callerId, obj.courseName);
+    if(userId == null) {
+        userId = getSamePeriodCourseUser(obj.period, obj.day);
+    }
+
+    return userId;
+}
