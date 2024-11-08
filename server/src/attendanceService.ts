972ef018cc95fa84f001826d3d0292c32f1a8aab
10Hyuna <kimha105@naver.com>
2024-11-08 21:16:50 +0900

    refactor: 유저 리스트를 매개변수로 해서 랜덤하게 유저를 선택하는 로직 함수화

--- server/src/attendanceService.ts
+++ server/src/attendanceService.ts
@@ -1,19 +1,29 @@
-import { getUsersInSameCourse } from './db'; 
+import { getUsersInSameCourse, getUsersInSameTime } from './db'; 
 
-interface Course {
-    course_name: string;
-    course_number: string;
-    class_number: string;
-    period: string;
-    day: string;
-    caller_id: string;
-}
+export async function getSameCourseUser(
+    callerId: string, 
+    courseName: string
+  ): Promise<string | null> {
+    const userIds = await getUsersInSameCourse(callerId, courseName);
+    
+    return getRandomUser(userIds);
+  }
 
-export function getRandomUser(
-    userIds: string[]
-): string | null {
-    if (userIds.length === 0) return null;
+  export async function getSamePeriodCourseUser(
+    period: string,
+    day: string
+  ): Promise<string | null> {
+    const userIds = await getUsersInSameTime(period, day);
+    
+    return getRandomUser(userIds);
+  }
+
+function getRandomUser(
+    userIds: string[],
+): string | null  {
+    if(userIds.length == 0) return null
 
     const randomIndex = Math.floor(Math.random() * userIds.length);
+
     return userIds[randomIndex];
-}
+  }
